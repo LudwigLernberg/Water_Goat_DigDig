9ccf77f6ba2e7399bbe02a0f25ea6c20fdf7c0d8
ViggoWarnehall <156196617+ViggoWarnehall@users.noreply.github.com>
2025-10-16 11:45:33 +0200

    bättre hopp

--- Water_Goat_DigDig02/Assets/scripts/jump.cs
+++ Water_Goat_DigDig02/Assets/scripts/jump.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -36,4 +36,57 @@ public class jump : MonoBehaviour
     }
 
    
+}*/
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    [Header("Jump Settings")]
+    public float jumpForce = 10f;
+    public LayerMask groundMask;
+
+    private Rigidbody rb;
+    private bool isGrounded = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Hoppa bara när vi är på marken
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            JumpAction();
+        }
+    }
+
+    void JumpAction()
+    {
+        // Nollställ eventuell vertikal hastighet för konsekventa hopp
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // Lägg till hoppkraft
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        // Kolla om vi nuddar något på marklagret
+        if (((1 << collision.gameObject.layer) & groundMask) != 0)
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        // Om vi inte längre nuddar marken
+        if (((1 << collision.gameObject.layer) & groundMask) != 0)
+        {
+            isGrounded = false;
+        }
+    }
 }
+
