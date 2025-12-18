b23f716121741d04039c79caf7cdb1f074053db3
RasmusG07 <rasmus.gustafsson2@elev.ga.lbs.se>
2025-12-18 10:55:23 +0100

    dsa

--- Water_Goat_DigDig02/Assets/Rasmus/XXMovementUnity.cs
+++ Water_Goat_DigDig02/Assets/Rasmus/XXMovementUnity.cs
@@ -1,39 +1,43 @@
 using UnityEngine;
 
-public class PlayerJump : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+public class SnappyJump : MonoBehaviour
 {
-    public float jumpForce = 7f;
-    public LayerMask groundLayer;
+    [Header("Jump Settings")]
+    public float jumpForce = 7f;   // Adjust for jump height
+    public LayerMask groundLayer;  // Layer for ground detection
+    public float groundCheckDistance = 0.1f;
 
     private Rigidbody rb;
     private bool isGrounded;
 
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true; // Prevent tipping over
     }
 
     void Update()
     {
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        CheckGrounded();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             Jump();
         }
     }
 
-    void FixedUpdate()
-    {
-        CheckGrounded();
-    }
-
     void Jump()
     {
+        // Reset vertical velocity for consistent jumps
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
         rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 
     void CheckGrounded()
     {
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f, groundLayer);
+        // Raycast down to detect ground
+        isGrounded = Physics.Raycast(transform.position, Vector3.down,
+                                     groundCheckDistance + 0.1f, groundLayer);
     }
 }
